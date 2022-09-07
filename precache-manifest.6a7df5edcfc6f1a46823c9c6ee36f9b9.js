e9fdd4d5dd8d93f4608ecc55b2d4d2d17050ee4e
DenofProjects <denofprojects@hotmail.com>
2022-09-07 18:16:26 +0530

    Updates

---
+++ precache-manifest.6a7df5edcfc6f1a46823c9c6ee36f9b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ca5de2bf7b0e7991a4ccd9af73eaa21",
+    "url": "/intrinsic-value-calculator/index.html"
+  },
+  {
+    "revision": "e7248e627e78077774fd",
+    "url": "/intrinsic-value-calculator/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "213124a1265a9d14b2ec",
+    "url": "/intrinsic-value-calculator/static/css/main.a960fb8e.chunk.css"
+  },
+  {
+    "revision": "e7248e627e78077774fd",
+    "url": "/intrinsic-value-calculator/static/js/2.bc23b269.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/intrinsic-value-calculator/static/js/2.bc23b269.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "213124a1265a9d14b2ec",
+    "url": "/intrinsic-value-calculator/static/js/main.e83f0888.chunk.js"
+  },
+  {
+    "revision": "afa4acd48ce490c7d8e8",
+    "url": "/intrinsic-value-calculator/static/js/runtime-main.3851bf3e.js"
+  }
+]);
